0611e774e29779b00ce59807e079b73f167fa7e3
Michael Douglas <mikedgls17@gmail.com>
2023-01-07 15:06:22 -0300

    javascript inicial

--- 03-calculadora-de-imc/js/script.js
+++ 03-calculadora-de-imc/js/script.js
@@ -4,7 +4,38 @@ const btnCalculate = document.getElementById("btn-calculate");
 const btnClean = document.getElementById("btn-clean");
 const imcResult = document.getElementById("imc-result");
 
-btnCalculate.addEventListener("click", ()=>{
-    const imc = weight.value / (height.value ** 2);
-    imcResult.innerText = imc;
-})
+btnCalculate.addEventListener("click", () => {
+  const imc = (
+    weight.value.replace(",", ".") /
+    height.value.replace(",", ".") ** 2
+  ).toFixed(2);
+  imcResult.innerText = `${imc} (${classificacaoImc(imc)})`;
+});
+
+btnClean.addEventListener("click", () => {
+  height.value = "";
+  weight.value = "";
+  imcResult.innerText = "";
+});
+
+height.addEventListener("keypress", (event) => {
+  if (height.value.length === 4) event.preventDefault();
+});
+
+weight.addEventListener("keypress", (event) => {
+  if (weight.value.length === 6) event.preventDefault();
+});
+
+function addPontuacao(campo, posicao) {
+  if (campo.value.length === --posicao) {
+    campo.value += ",";
+  }
+}
+
+function classificacaoImc(imc) {
+  if (imc < 18.5) return "Magreza";
+  if (imc <= 24.9) return "Normal";
+  if (imc <= 29.9) return "Sobrepreso";
+  if (imc <= 39.9) return "Obesidade";
+  return "Obesidade grave";
+}
